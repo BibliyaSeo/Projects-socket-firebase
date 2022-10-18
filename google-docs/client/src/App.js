faf2ae8f2be6ce0176da7347cfcbbd5626adfe4e
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-18 10:47:06 +0900

    Finish: google-docs

--- google-docs/client/src/App.js
+++ google-docs/client/src/App.js
@@ -1,7 +1,16 @@
 import TextEditor from "./TextEditor";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
 function App() {
-  return <TextEditor />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- google-docs/server/server.js
+++ google-docs/server/server.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect("mongodb://localhost/google-docs"); // 나중에 디비 연결 필요(?)
+
 const io = require("socket.io")(3001, {
   cors: {
     origin: "http://localhost:3000",
@@ -5,6 +10,27 @@ const io = require("socket.io")(3001, {
   },
 });
 
+const defaultValue = "";
+
 io.on("connection", (socket) => {
-  //여기부터 해야함
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
 });
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
