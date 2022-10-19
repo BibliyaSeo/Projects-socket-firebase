e6c7406525e4408fd92935f265121aa7368edc85
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-19 18:24:56 +0900

    Update: 화면 구성 및 기능 추가

--- sk-chat/server/index.js
+++ sk-chat/server/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", userRoutes);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on Port ${process.env.PORT}`);
+});
