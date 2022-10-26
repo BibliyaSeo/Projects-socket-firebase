527ac56dfb8d931a047364d25486b38d80f7cdea
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-26 15:13:05 +0900

    Update: 화면 순서 변경

--- dis-zoom/src/public/js/app.js
+++ dis-zoom/src/public/js/app.js
@@ -1,6 +1,7 @@
 const socket = io();
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const nickNameForm = welcome.querySelector("#nickname");
+const roomNumberForm = welcome.querySelector("#roomNumber");
 const room = document.getElementById("room");
 
 room.hidden = true;
@@ -16,7 +17,7 @@ const addMessage = (message) => {
 
 const handleMessageSubmit = (e) => {
   e.preventDefault();
-  const input = room.querySelector("input");
+  const input = room.querySelector("#msg input");
   const value = input.value;
   socket.emit("new_message", input.value, roomName, () => {
     addMessage(`You: ${value}`);
@@ -24,31 +25,38 @@ const handleMessageSubmit = (e) => {
   input.value = "";
 };
 
+const handleNicknameSubmit = (e) => {
+  e.preventDefault();
+  const input = nickNameForm.querySelector("input");
+  socket.emit("nickname", input.value);
+};
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
 };
 
 const handleRoomSubmit = (e) => {
   e.preventDefault();
-  const input = form.querySelector("input");
+  const input = roomNumberForm.querySelector("input");
   roomName = input.value;
   socket.emit("enter_room", input.value, showRoom);
   input.value = "";
 };
 
-form.addEventListener("submit", handleRoomSubmit);
+nickNameForm.addEventListener("submit", handleNicknameSubmit);
+roomNumberForm.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-  addMessage("Someone joined");
+socket.on("welcome", (user) => {
+  addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", () => {
-  addMessage("Someone left ㅠㅠ");
+socket.on("bye", (user) => {
+  addMessage(`${user} left!`);
 });
 
 socket.on("new_message", addMessage);
